64a133fd5976eac62ab23790325c92eec10535ed
Arm_com <kantawitsakhet@gmail.com>
2021-10-29 12:47:42 +0700

    port 80

--- Final_database.js
+++ Final_database.js
@@ -90,9 +90,9 @@ app.get("/piechart", async(req, res)=>{
     res.send(data.recordsets)
 })
 
-const PORT = process.env.PORT || 80;
+// const PORT = process.env.PORT || 80;
 
 
-server.listen(PORT,(req,res)=>{
+server.listen(80,(req,res)=>{
     console.log("ok")
 })
